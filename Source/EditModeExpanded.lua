843f7ef8daacc8724005992e329fb9d9d6067c6a
ced <ced8120@gmail.com>
2022-11-01 19:36:27 +0100

    Do the logic only when the addon is loaded

--- Source/EditModeExpanded.lua
+++ Source/EditModeExpanded.lua
@@ -1,14 +1,17 @@
 local lib = LibStub:GetLibrary("EditModeExpanded-1.0")
 
 local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", function()
-    if not EditModeExpandedDB then EditModeExpandedDB = {} end
-    if not EditModeExpandedDB.MicroButtonAndBagsBar then EditModeExpandedDB.MicroButtonAndBagsBar = {} end
-    if not EditModeExpandedDB.StatusTrackingBarManager then EditModeExpandedDB.StatusTrackingBarManager = {} end
-    if not EditModeExpandedDB.QueueStatusButton then EditModeExpandedDB.QueueStatusButton = {} end
-    
-    lib.RegisterFrame(MicroButtonAndBagsBar, "Micro Menu", EditModeExpandedDB.MicroButtonAndBagsBar)
-    lib.RegisterFrame(StatusTrackingBarManager, "Experience Bar", EditModeExpandedDB.StatusTrackingBarManager)
-    lib.RegisterFrame(QueueStatusButton, "LFG", EditModeExpandedDB.QueueStatusButton)
+f:SetScript("OnEvent", function(__, event, arg1)
+    if (event == "ADDON_LOADED" and arg1 == "EditModeExpanded") then
+        if not EditModeExpandedDB then EditModeExpandedDB = {} end
+        if not EditModeExpandedDB.MicroButtonAndBagsBar then EditModeExpandedDB.MicroButtonAndBagsBar = {} end
+        if not EditModeExpandedDB.StatusTrackingBarManager then EditModeExpandedDB.StatusTrackingBarManager = {} end
+        if not EditModeExpandedDB.QueueStatusButton then EditModeExpandedDB.QueueStatusButton = {} end
+
+        lib.RegisterFrame(MicroButtonAndBagsBar, "Micro Menu", EditModeExpandedDB.MicroButtonAndBagsBar)
+        lib.RegisterFrame(StatusTrackingBarManager, "Experience Bar", EditModeExpandedDB.StatusTrackingBarManager)
+        lib.RegisterFrame(QueueStatusButton, "LFG", EditModeExpandedDB.QueueStatusButton)
+    end
 end)
+
+f:RegisterEvent("ADDON_LOADED")

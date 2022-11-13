5c5b2adbd7b41d9f654a0c97df3fda487bcefa96
Teelo <49792789+teelolws@users.noreply.github.com>
2022-11-13 19:27:28 +1300

    Version 17

--- Source/libs/EditModeExpanded-1.0/EditModeExpanded-1.0.lua
+++ Source/libs/EditModeExpanded-1.0/EditModeExpanded-1.0.lua
@@ -3,7 +3,7 @@
 --
 
 local CURRENT_BUILD = "10.0.0"
-local MAJOR, MINOR = "EditModeExpanded-1.0", 16
+local MAJOR, MINOR = "EditModeExpanded-1.0", 17
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 

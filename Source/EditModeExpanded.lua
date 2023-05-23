1b7f568bbed39cfeaf583b505a34f5dd77f9ad47
Teelo <49792789+teelolws@users.noreply.github.com>
2023-05-24 10:21:09 +1200

    Raid manager more restrictions - no changes while in combat

--- Source/EditModeExpanded.lua
+++ Source/EditModeExpanded.lua
@@ -872,6 +872,7 @@ f:SetScript("OnEvent", function(__, event, arg1)
             lib:RegisterFrame(CompactRaidFrameManager, "Raid Manager", db.CompactRaidFrameManager)
             local expanded
             hooksecurefunc("CompactRaidFrameManager_Expand", function()
+                if InCombatLockdown() then return end
                 if expanded then return end
                 expanded = true
                 CompactRaidFrameManager:ClearPoint("TOPLEFT")
@@ -883,6 +884,7 @@ f:SetScript("OnEvent", function(__, event, arg1)
                 end
             end)
             hooksecurefunc("CompactRaidFrameManager_Collapse", function()
+                if InCombatLockdown() then return end
                 if not expanded then return end
                 expanded = false
                 CompactRaidFrameManager:ClearPoint("TOPLEFT")
@@ -893,6 +895,7 @@ f:SetScript("OnEvent", function(__, event, arg1)
             
             -- the wasVisible saved in the library when entering Edit Mode cannot be relied upon, as entering Edit Mode shows the raid manager in some situations, before we can detect if it was already visible
             hooksecurefunc(EditModeManagerFrame, "ExitEditMode", function()
+                if InCombatLockdown() then return end
                 CompactRaidFrameManager:SetShown(IsInGroup() or IsInRaid())
             end)
             
@@ -900,10 +903,7 @@ f:SetScript("OnEvent", function(__, event, arg1)
                 local noInfinite
                 hooksecurefunc(CompactRaidFrameManager, "SetShown", function()
                     if noInfinite then return end
-                    if InCombatLockdown() then
-                        local _, anchor = CompactRaidFrameContainer:GetPoint(1)
-                        if anchor == CompactRaidFrameManager then return end
-                    end
+                    if InCombatLockdown() then return end
                     if EditModeManagerFrame.editModeActive then
                         CompactRaidFrameManager:Show()
                     else

b83e5a95dd73cad61d925215aac68e8f92453225
Teelo <49792789+teelolws@users.noreply.github.com>
2023-05-30 10:21:21 +1200

    Make combo point frame a bit more persistent about staying hidden

--- Source/EditModeExpanded.lua
+++ Source/EditModeExpanded.lua
@@ -817,6 +817,13 @@ f:SetScript("OnEvent", function(__, event, arg1)
                         lib:RepositionFrame(RogueComboPointBarFrame)
                     end
                 end)
+                local noInfinite
+                hooksecurefunc(RogueComboPointBarFrame, "Show", function()
+                    if noInfinite then return end
+                    noInfinite = true
+                    lib:RepositionFrame(RogueComboPointBarFrame)
+                    noInfinite = false
+                end)
             end
         elseif class == "PRIEST" then
             -- shadowfiend uses totem frame
@@ -834,6 +841,12 @@ f:SetScript("OnEvent", function(__, event, arg1)
                     if not EditModeManagerFrame.editModeActive then
                         lib:RepositionFrame(DruidComboPointBarFrame)
                     end
+                end)
+                hooksecurefunc(DruidComboPointBarFrame, "Show", function()
+                    if noInfinite then return end
+                    noInfinite = true
+                    lib:RepositionFrame(DruidComboPointBarFrame)
+                    noInfinite = false
                 end)
             end
         end

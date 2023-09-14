51f745fb688930eb4b04285af601c2361dd11332
Teelo <49792789+teelolws@users.noreply.github.com>
2023-09-15 10:57:54 +1200

    Code cleanup: Move registerSecureFrameHideable to dedicated file

--- Source/RegisteredSystemFrames.lua
+++ Source/RegisteredSystemFrames.lua
@@ -24,3 +24,82 @@ function addon:initSystemFrames()
     -- Since its actually not normally shown on login, we will immediately re-hide it again.
     TalkingHeadFrame:Hide()
 end
+
+function addon:registerSecureFrameHideable(frame)
+    local hidden, toggleInCombat, x, y
+    
+    local function hide()
+        if not x then
+            x, y = frame:GetLeft(), frame:GetBottom()
+        end
+        frame:ClearAllPoints()
+        frame:SetClampedToScreen(false)
+        frame:SetPoint("TOPRIGHT", UIParent, "BOTTOMLEFT", -1000, -1000)
+    end
+    
+    local function show()
+        if not x then return end
+        frame:ClearAllPoints()
+        frame:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", x, y)
+        x, y = nil, nil
+    end
+    
+    EventRegistry:RegisterFrameEventAndCallbackWithHandle("PLAYER_REGEN_ENABLED", function()
+        if not toggleInCombat then return end
+        if hidden then
+            hide()
+        else
+            show()
+        end
+    end)
+    EventRegistry:RegisterFrameEventAndCallbackWithHandle("PLAYER_REGEN_DISABLED", function()
+        if not toggleInCombat then return end
+        if hidden then
+            show()
+        else
+            hide()
+        end
+    end)
+    
+    lib:RegisterCustomCheckbox(frame, "Hide",
+        function()
+            hidden = true
+            if not EditModeManagerFrame.editModeActive then
+                hide()
+            end
+        end,
+        function()
+            hidden = false
+            show()
+        end,
+        "HidePermanently")
+    
+    lib:RegisterCustomCheckbox(frame, "Toggle In Combat",
+        function()
+            toggleInCombat = true
+        end,
+        function()
+            toggleInCombat = false
+        end,
+        "ToggleInCombat")
+    
+    hooksecurefunc(EditModeManagerFrame, "EnterEditMode", function()
+        show()
+    end)
+    
+    hooksecurefunc(EditModeManagerFrame, "ExitEditMode", function()
+        if hidden then
+            hide()
+        end
+    end)
+    
+    hooksecurefunc(frame, "Show", function()
+        if InCombatLockdown() then return end
+        if hidden then hide() end
+    end)
+    
+    hooksecurefunc(frame, "SetShown", function()
+        if InCombatLockdown() then return end
+        if hidden then hide() end
+    end)
+end

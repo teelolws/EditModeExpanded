ca141d8cc56ec5c4ebb11c3f8f1595b2cbd65714
Teelo <49792789+teelolws@users.noreply.github.com>
2024-08-19 16:02:19 +1200

    Fix Druid combo points issue
    Closes: https://github.com/teelolws/EditModeExpanded/issues/138

--- Source/FrameHandlers/ClassResources/ComboPoints.lua
+++ Source/FrameHandlers/ClassResources/ComboPoints.lua
@@ -36,6 +36,7 @@ function addon:initDruidComboPoints()
         lib:RegisterResizable(DruidComboPointBarFrame)
         addon.registerAnchorToDropdown(DruidComboPointBarFrame)
         hooksecurefunc(PlayerFrameBottomManagedFramesContainer, "Layout", function()
+            if not DruidComboPointBarFrame:ShouldShowBar() then return end
             if not EditModeManagerFrame.editModeActive then
                 lib:RepositionFrame(DruidComboPointBarFrame)
             end
@@ -43,6 +44,7 @@ function addon:initDruidComboPoints()
         local noInfinite
         hooksecurefunc(DruidComboPointBarFrame, "Show", function()
             if noInfinite then return end
+            if not DruidComboPointBarFrame:ShouldShowBar() then return end
             noInfinite = true
             lib:RepositionFrame(DruidComboPointBarFrame)
             noInfinite = false

55ae692e28339ff228803a7c69ea179149521885
Teelo <49792789+teelolws@users.noreply.github.com>
2024-08-26 14:05:38 +1200

    Update GameMenu.lua

--- Source/FrameHandlers/GameMenu.lua
+++ Source/FrameHandlers/GameMenu.lua
@@ -6,14 +6,13 @@ function addon:initGameMenu()
     local db = addon.db.global
     if not db.EMEOptions.gameMenu then return end
     local loaded
-    hooksecurefunc(GameMenuFrame, "Show", function()
-        RunNextFrame(function()
-            if loaded then return end
-            loaded = true
-            lib:RegisterFrame(GameMenuFrame, "Game Menu", db.GameMenuFrame)
-            lib:RegisterResizable(GameMenuFrame)
-            lib:HideByDefault(GameMenuFrame)
-            GameMenuFrame.Selection:SetFrameStrata("DIALOG")
-        end)
+    hooksecurefunc(GameMenuFrame, "Layout", function()
+        if loaded then return end
+        if InCombatLockdown() then return end
+        loaded = true
+        lib:RegisterFrame(GameMenuFrame, "Game Menu", db.GameMenuFrame)
+        lib:RegisterResizable(GameMenuFrame)
+        lib:HideByDefault(GameMenuFrame)
+        GameMenuFrame.Selection:SetFrameStrata("DIALOG")
     end)
 end

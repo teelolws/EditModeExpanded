19d837d15a55ab208ffbd919fee0898ff6fd96d1
Teelo <49792789+teelolws@users.noreply.github.com>
2025-04-17 10:19:04 +1200

    Add option to hide Loss of Control background

--- Source/FrameHandlers/LossOfControl.lua
+++ Source/FrameHandlers/LossOfControl.lua
@@ -4,16 +4,29 @@ local lib = LibStub:GetLibrary("EditModeExpanded-1.0")
 
 function addon:initLossOfControl()
     local db = addon.db.global
-    if db.EMEOptions.lossOfControl then
-        lib:RegisterFrame(LossOfControlFrame, LOSS_OF_CONTROL, db.LOC)
-        lib:HideByDefault(LossOfControlFrame)
-        hooksecurefunc(EditModeManagerFrame, "EnterEditMode", function(self)
-            if InCombatLockdown() then return end
-            LossOfControlFrame:SetScript("OnUpdate", nop)
-        end)
-        hooksecurefunc(EditModeManagerFrame, "ExitEditMode", function(self)
-            if InCombatLockdown() then return end
-            LossOfControlFrame:SetScript("OnUpdate", LossOfControlFrame_OnUpdate)
-        end)
-    end
+    if not db.EMEOptions.lossOfControl then return end
+    lib:RegisterFrame(LossOfControlFrame, LOSS_OF_CONTROL, db.LOC)
+    lib:HideByDefault(LossOfControlFrame)
+    hooksecurefunc(EditModeManagerFrame, "EnterEditMode", function(self)
+        if InCombatLockdown() then return end
+        LossOfControlFrame:SetScript("OnUpdate", nop)
+    end)
+    hooksecurefunc(EditModeManagerFrame, "ExitEditMode", function(self)
+        if InCombatLockdown() then return end
+        LossOfControlFrame:SetScript("OnUpdate", LossOfControlFrame_OnUpdate)
+    end)
+    
+    lib:RegisterCustomCheckbox(LossOfControlFrame, "Hide Glow Effect",
+        function()
+            LossOfControlFrame.RedLineBottom:Hide()
+            LossOfControlFrame.RedLineTop:Hide()
+            LossOfControlFrame.blackBg:Hide()
+        end,
+        function()
+            LossOfControlFrame.RedLineBottom:Show()
+            LossOfControlFrame.RedLineTop:Show()
+            LossOfControlFrame.blackBg:Show()
+        end,
+        "HideIcons"
+    )
 end

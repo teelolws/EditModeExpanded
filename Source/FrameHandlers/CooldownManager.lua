7197af58c000cb2985f5f335539fa5daaac8b0df
Teelo <49792789+teelolws@users.noreply.github.com>
2025-04-23 16:40:48 +1200

    Cooldown manager: Add option to move icons around

--- Source/FrameHandlers/CooldownManager.lua
+++ Source/FrameHandlers/CooldownManager.lua
@@ -6,13 +6,36 @@ local libDD = LibStub:GetLibrary("LibUIDropDownMenu-4.0")
 
 local function initFrame(frame)
     local dropdown, getSettingDB = lib:RegisterDropdown(frame, libDD, "ExcludeCooldownDropdown")
-
+    local rearrangeDropdown, getRearrangeSettingDB = lib:RegisterDropdown(frame, libDD, "RearrangeCooldownDropdown")
+    
     hooksecurefunc(frame, "RefreshData", function(self)
-        if self:IsEditing() then return end
+        --if self:IsEditing() then return end
         
     	local cooldownIDs = self:GetCooldownIDs();
         
         local db = getSettingDB()
+        local rdb = getRearrangeSettingDB()
+        
+        local cooldownIDsSorting = {}
+        for cooldownIndex, cooldownID in ipairs(cooldownIDs) do
+            local index = cooldownIndex
+            if rdb[cooldownID] then
+                index = index + rdb[cooldownID]
+            end
+            table.insert(cooldownIDsSorting, {id = cooldownID, index = index})
+        end
+        table.sort(cooldownIDsSorting, function(a, b)
+            if a.index == b.index then
+                return a.id < b.id
+            end
+            return a.index < b.index
+        end)
+        
+        wipe(cooldownIDs)
+        for index, cooldownInfo in pairs(cooldownIDsSorting) do
+            table.insert(cooldownIDs, cooldownInfo.id)
+        end
+        
         for cooldownID in pairs(db) do
             for i = #cooldownIDs, 1, -1 do
                 if cooldownIDs[i] == cooldownID then
@@ -58,6 +81,42 @@ local function initFrame(frame)
     end)
     libDD:UIDropDownMenu_SetWidth(dropdown, 200)
     libDD:UIDropDownMenu_SetText(dropdown, "Hide These Spells")
+    
+    libDD:UIDropDownMenu_Initialize(rearrangeDropdown, function(self, level, menuList)
+        local db = getRearrangeSettingDB()
+        local info = libDD:UIDropDownMenu_CreateInfo()        
+        local cooldownIDs = frame:GetCooldownIDs()
+        
+        for cooldownIndex, cooldownID in ipairs(cooldownIDs) do
+            local cooldownInfo = C_CooldownViewer.GetCooldownViewerCooldownInfo(cooldownID)
+            local name = C_Spell.GetSpellName(cooldownInfo.spellID)
+            
+            info.text = "Move "..name.." ("..(cooldownIndex + (db[cooldownID] or 0))..")".." Up"
+            info.func = function()
+                if db[cooldownID] then
+                    db[cooldownID] = db[cooldownID] - 1
+                else
+                    db[cooldownID] = cooldownIndex - 1
+                end
+                frame:RefreshData()
+            end
+            libDD:UIDropDownMenu_AddButton(info)
+            
+            info.text = "Move "..name.." Down"
+            info.func = function()
+                if db[cooldownID] then
+                    db[cooldownID] = db[cooldownID] + 1
+                else
+                    db[cooldownID] = cooldownIndex + 1
+                end
+                frame:RefreshData()
+            end
+            libDD:UIDropDownMenu_AddButton(info)
+        end
+    end)
+    libDD:UIDropDownMenu_SetWidth(rearrangeDropdown, 200)
+    libDD:UIDropDownMenu_SetText(rearrangeDropdown, "Rearrange These Spells")
+    
 end
 
 function addon:initCooldownManager()

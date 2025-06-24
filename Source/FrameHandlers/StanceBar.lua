58d6e7ef771e161b07c022128a24e15ce3229c78
Teelo <49792789+teelolws@users.noreply.github.com>
2025-06-24 23:08:35 +1200

    Update StanceBar.lua

--- Source/FrameHandlers/StanceBar.lua
+++ Source/FrameHandlers/StanceBar.lua
@@ -5,26 +5,6 @@ local lib = LibStub:GetLibrary("EditModeExpanded-1.0")
 function addon:initStanceBar()
     local db = addon.db.global
     if db.EMEOptions.stanceBar then
-        lib:RegisterHideable(StanceBar)
-        lib:RegisterToggleInCombat(StanceBar)
-        hooksecurefunc(StanceBar, "Show", function()
-            if InCombatLockdown() then return end
-            if lib:IsFrameMarkedHidden(StanceBar) then
-                StanceBar:Hide()
-            end
-        end)
-        hooksecurefunc(StanceBar, "SetShown", function()
-            if InCombatLockdown() then return end
-            if lib:IsFrameMarkedHidden(StanceBar) then
-                StanceBar:Hide()
-            end
-        end)
-        
-        C_Timer.After(1, function()
-            if InCombatLockdown() then return end
-            if lib:IsFrameMarkedHidden(StanceBar) then
-                StanceBar:Hide()
-            end
-        end)
+        addon:registerSecureFrameHideable(StanceBar)
     end
 end

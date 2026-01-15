77cee042aa12fbc0baa4797b80c64d2fb8fc7859
Teelo <49792789+teelolws@users.noreply.github.com>
2026-01-15 14:43:16 +1300

    Update ChatButtons-tbc.lua

--- Source/FrameHandlers/ChatButtons-tbc.lua
+++ Source/FrameHandlers/ChatButtons-tbc.lua
@@ -1,8 +1,5 @@
 local addonName, addon = ...
 
-local L = LibStub("AceLocale-3.0"):GetLocale(addonName)
-local lib = LibStub:GetLibrary("EditModeExpanded-1.0")
-
 function addon:initChatButtons()
     local db = addon.db.global
     if not db.EMEOptions.chatButtons then return end

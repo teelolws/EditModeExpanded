6c008cf57084774381273ab33cb0b3fc9a603e40
Teelo <49792789+teelolws@users.noreply.github.com>
2026-01-22 20:14:19 +1300

    Make Objective Tracker resizable

--- Source/FrameHandlers/ObjectiveTracker.lua
+++ Source/FrameHandlers/ObjectiveTracker.lua
@@ -1,8 +1,11 @@
 local addonName, addon = ...
 
+local lib = LibStub:GetLibrary("EditModeExpanded-1.0")
+
 function addon:initObjectiveTracker()
     local db = addon.db.global
     if db.EMEOptions.objectiveTrackerFrame then
         addon:registerSecureFrameHideable(ObjectiveTrackerFrame)
+        lib:RegisterResizable(ObjectiveTrackerFrame)
     end
 end

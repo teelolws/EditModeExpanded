909567c10e43dbfe49e3d64c03b49f49bc5db55e
Teelo <49792789+teelolws@users.noreply.github.com>
2026-03-08 07:42:25 +1300

    Update StatusTrackingBar.lua

--- Source/FrameHandlers/StatusTrackingBar.lua
+++ Source/FrameHandlers/StatusTrackingBar.lua
@@ -8,63 +8,11 @@ function addon:initStatusTrackingBar()
         lib:RegisterResizable(MainStatusTrackingBarContainer)
         lib:RegisterHideable(MainStatusTrackingBarContainer)
         lib:RegisterToggleInCombat(MainStatusTrackingBarContainer)
-        C_Timer.After(1, function() lib:UpdateFrameResize(MainStatusTrackingBarContainer) end)
-        hooksecurefunc(MainStatusTrackingBarContainer, "SetScale", function(_, scale)
-            for _, bar in ipairs(StatusTrackingBarManager.barContainers) do
-                local _, anchor = bar:GetPoint(1)
-                if anchor == MainStatusTrackingBarContainer then
-                    bar:SetScale(scale) 
-                end
-            end
-        end)
-        hooksecurefunc(MainStatusTrackingBarContainer, "SetScaleOverride", function(_, scale)
-            for _, bar in ipairs(StatusTrackingBarManager.barContainers) do
-                local _, anchor = bar:GetPoint(1)
-                if anchor == MainStatusTrackingBarContainer then
-                    bar:SetScale(scale) 
-                end
-            end
-        end)
-        
-        hooksecurefunc(StatusTrackingBarManager, "UpdateBarsShown", function()
-            for _, bar in ipairs(StatusTrackingBarManager.barContainers) do
-                local _, anchor = bar:GetPoint(1)
-                if anchor == MainStatusTrackingBarContainer then
-                    bar:SetScale(MainStatusTrackingBarContainer:GetScale()) 
-                end
-            end
-        end)
     end
     
     if db.EMEOptions.secondaryStatusTrackingBarContainer then
         lib:RegisterResizable(SecondaryStatusTrackingBarContainer)
         lib:RegisterHideable(SecondaryStatusTrackingBarContainer)
         lib:RegisterToggleInCombat(SecondaryStatusTrackingBarContainer)
-        C_Timer.After(1, function() lib:UpdateFrameResize(SecondaryStatusTrackingBarContainer) end)
-        hooksecurefunc(SecondaryStatusTrackingBarContainer, "SetScale", function(_, scale)
-            for _, bar in ipairs(StatusTrackingBarManager.barContainers) do
-                local _, anchor = bar:GetPoint(1)
-                if anchor == SecondaryStatusTrackingBarContainer then
-                    bar:SetScale(scale) 
-                end
-            end
-        end)
-        hooksecurefunc(SecondaryStatusTrackingBarContainer, "SetScaleOverride", function(_, scale)
-            for _, bar in ipairs(StatusTrackingBarManager.barContainers) do
-                local _, anchor = bar:GetPoint(1)
-                if anchor == SecondaryStatusTrackingBarContainer then
-                    bar:SetScale(scale) 
-                end
-            end
-        end)
-        
-        hooksecurefunc(StatusTrackingBarManager, "UpdateBarsShown", function()
-            for _, bar in ipairs(StatusTrackingBarManager.barContainers) do
-                local _, anchor = bar:GetPoint(1)
-                if anchor == SecondaryStatusTrackingBarContainer then
-                    bar:SetScale(SecondaryStatusTrackingBarContainer:GetScale()) 
-                end
-            end
-        end)
     end
 end

c8b88afbecfa73c1caf7a4c8728e09db0ae4cc6f
Teelo <49792789+teelolws@users.noreply.github.com>
2026-06-28 08:41:17 +1200

    Add options to hide target buffs and debuffs

--- Source/FrameHandlers/TargetFrame.lua
+++ Source/FrameHandlers/TargetFrame.lua
@@ -13,8 +13,15 @@ function addon:initTargetFrame()
             addon:registerFrame(targetBuffsFrame, "Target Buffs", db.TargetBuffs)
             lib:SetDontResize(targetBuffsFrame)
             
+            local targetDebuffsHidden, targetBuffsHidden
+            
             hooksecurefunc("TargetFrame_UpdateDebuffAnchor", function(self, buff)
                 if self ~= TargetFrame then return end
+                
+                if targetDebuffsHidden then
+                    buff:Hide()
+                    return
+                end
                 
                 local point, relativeTo, relativePoint, offsetX, offsetY = buff:GetPoint()
                 
@@ -26,12 +33,37 @@ function addon:initTargetFrame()
             hooksecurefunc("TargetFrame_UpdateBuffAnchor", function(self, buff)
                 if self ~= TargetFrame then return end
                 
+                if targetBuffsHidden then
+                    buff:Hide()
+                    return
+                end
+                
                 local point, relativeTo, relativePoint, offsetX, offsetY = buff:GetPoint()
                 
                 if point and (self.TargetFrameContainer.FrameTexture == relativeTo) then
                     buff:SetPoint(point, targetBuffsFrame, relativePoint, offsetX, offsetY)
                 end
             end)
+            
+            lib:RegisterCustomCheckbox(targetBuffsFrame, "Hide Buffs",
+                function()
+                    targetBuffsHidden = true
+                end,
+                function()
+                    targetBuffsHidden = false
+                end,
+                "HideBuffs"
+            )
+            
+            lib:RegisterCustomCheckbox(targetBuffsFrame, "Hide Debuffs",
+                function()
+                    targetDebuffsHidden = true
+                end,
+                function()
+                    targetDebuffsHidden = false
+                end,
+                "HideDebuffs"
+            )
         end
         
         local targetFrameWasHidden

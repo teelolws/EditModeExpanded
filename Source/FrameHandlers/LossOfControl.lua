d8acba8e1cbdd61bfa0c7956ddf70fe7e2cb0133
Teelo <49792789+teelolws@users.noreply.github.com>
2026-08-12 10:23:42 +1200

    Update LossOfControl.lua

--- Source/FrameHandlers/LossOfControl.lua
+++ Source/FrameHandlers/LossOfControl.lua
@@ -6,20 +6,6 @@ local lib = LibStub:GetLibrary("EditModeExpanded-1.0")
 function addon:initLossOfControl()
     local db = addon.db.global
     if not db.EMEOptions.lossOfControl then return end
-    addon:registerFrame(LossOfControlFrame, LOSS_OF_CONTROL, db.LOC)
-    lib:HideByDefault(LossOfControlFrame)
-    
-    -- The OnUpdate event will try and hide the LOC Frame for not having any data
-    -- Suppress its OnUpdate handler during edit mode
-    hooksecurefunc(EditModeManagerFrame, "EnterEditMode", function(self)
-        if InCombatLockdown() then return end
-        LossOfControlFrame:SetScript("OnUpdate", nop)
-    end)
-    hooksecurefunc(EditModeManagerFrame, "ExitEditMode", function(self)
-        if InCombatLockdown() then return end
-        LossOfControlFrame:SetScript("OnUpdate", LossOfControlMixin.OnUpdate)
-    end)
-    
     lib:RegisterCustomCheckbox(LossOfControlFrame, L["HIDE_GLOW_EFFECT"],
         function()
             LossOfControlFrame.RedLineBottom:Hide()
